cd767d63b2c25320f3846f4fda494105e3576d57
Chris Su <249674239@qq.com>
2018-12-05 22:11:25 +0900

    app resource server

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -96,10 +96,6 @@ public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
 		
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Bean
 	public PersistentTokenRepository persistentTokenRepository() {
